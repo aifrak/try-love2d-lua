a110b0430c5bed978af9b73101315bcb3a32739b
aifrak <aifrak@users.noreply.github.com>
2024-12-22 14:02:10 +0100

    Make variable local

--- lib/main.lua
+++ lib/main.lua
@@ -1,12 +1,14 @@
+local x, y, w, h
+
 -- Load some default values for our rectangle.
 function love.load()
-  X, Y, W, H = 20, 20, 60, 20
+  x, y, w, h = 20, 20, 60, 20
 end
 
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
-  W = W + 1
-  H = H + 1
+  w = w + 1
+  h = h + 1
 end
 
 function love.draw()
@@ -14,5 +16,5 @@ function love.draw()
 
   -- Draw a coloured rectangle.
   love.graphics.setColor(0, 0.4, 0.4)
-  love.graphics.rectangle("fill", X, Y, W, H)
+  love.graphics.rectangle("fill", x, y, w, h)
 end
