14ce50599ec79dbab1476be8368c4303a67a4fe2
aifrak <aifrak@users.noreply.github.com>
2024-12-22 13:47:14 +0100

    Follow tutorial

--- lib/main.lua
+++ lib/main.lua
@@ -1,3 +1,18 @@
+-- Load some default values for our rectangle.
+function love.load()
+  X, Y, W, H = 20, 20, 60, 20
+end
+
+-- Increase the size of the rectangle every frame.
+function love.update(dt)
+  W = W + 1
+  H = H + 1
+end
+
 function love.draw()
   love.graphics.print("Hello World", 400, 300)
+
+  -- Draw a coloured rectangle.
+  love.graphics.setColor(0, 0.4, 0.4)
+  love.graphics.rectangle("fill", X, Y, W, H)
 end
