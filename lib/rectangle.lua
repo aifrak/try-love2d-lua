79e3c3f64abf8c2ec7a667e5780a636bbf0f8b3d
aifrak <aifrak@users.noreply.github.com>
2024-12-22 22:18:57 +0100

    Refactoring

--- lib/rectangle.lua
+++ lib/rectangle.lua
@@ -10,27 +10,4 @@ function rectangle.new()
   return rect
 end
 
-function rectangle:update(rect, dt)
-  if love.keyboard.isDown("right") then
-    rect.x = rect.x + 100 * dt
-  elseif love.keyboard.isDown("left") then
-    rect.x = rect.x - 100 * dt
-  end
-
-  return self
-end
-
-function rectangle:draw(rect)
-  love.graphics.setColor(0, 0.4, 0.4)
-  love.graphics.rectangle(
-    "fill",
-    rect.x,
-    rect.y,
-    rect.width,
-    rect.height
-  )
-
-  return self
-end
-
 return rectangle
--- lib/rectangles.lua
+++ lib/rectangles.lua
@@ -10,6 +10,23 @@ function rectangles:load()
   return self
 end
 
+function rectangles:createRectangle()
+  local rect = rectangle:new()
+  table.insert(tableRectangles, rect)
+
+  return self
+end
+
+function rectangle:update(rect, dt)
+  if love.keyboard.isDown("right") then
+    rect.x = rect.x + 100 * dt
+  elseif love.keyboard.isDown("left") then
+    rect.x = rect.x - 100 * dt
+  end
+
+  return self
+end
+
 function rectangles:updateAll(dt)
   for _, rect in ipairs(tableRectangles) do
     rectangle:update(rect, dt)
@@ -18,17 +35,23 @@ function rectangles:updateAll(dt)
   return self
 end
 
-function rectangles:drawAll()
-  for _, rect in ipairs(tableRectangles) do
-    rectangle:draw(rect)
-  end
+function rectangle:draw(rect)
+  love.graphics.setColor(0, 0.4, 0.4)
+  love.graphics.rectangle(
+    "fill",
+    rect.x,
+    rect.y,
+    rect.width,
+    rect.height
+  )
 
   return self
 end
 
-function rectangles:createRectangle()
-  local rect = rectangle:new()
-  table.insert(tableRectangles, rect)
+function rectangles:drawAll()
+  for _, rect in ipairs(tableRectangles) do
+    rectangle:draw(rect)
+  end
 
   return self
 end
