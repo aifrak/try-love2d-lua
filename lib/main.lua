764b10edbfbeadaa80f804550c10c43f7236d398
aifrak <aifrak@users.noreply.github.com>
2024-12-23 11:01:58 +0100

    Use example of tick module

--- lib/main.lua
+++ lib/main.lua
@@ -1,11 +1,19 @@
 local rectangles = require "rectangles"
+local tick = require "tick"
+
+local drawRectangle = false
 
 function love.load()
   rectangles:load()
+
+  --The first argument is a function
+  --The second argument is the time it takes to call the function
+  tick.delay(function() drawRectangle = true end, 2)
 end
 
 function love.update(dt)
   rectangles:updateAll(dt)
+  tick.update(dt)
 end
 
 function love.draw()
@@ -13,6 +21,12 @@ function love.draw()
   love.graphics.print("Hello World", 400, 300)
 
   rectangles:drawAll()
+
+  --if drawRectangle is true then draw a rectangle
+  if drawRectangle then
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.rectangle("fill", 100, 100, 300, 200)
+  end
 end
 
 function love.keypressed(key)
