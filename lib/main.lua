baa823ca28d6a50772045f310fa2d3f6eb4d3980
aifrak <aifrak@users.noreply.github.com>
2024-12-23 12:35:02 +0100

    Load an image

--- lib/main.lua
+++ lib/main.lua
@@ -2,8 +2,13 @@ local Rectangles = require "rectangles"
 local Tick = require "tick"
 
 local drawRectangle = false
+local myImage
 
 function love.load()
+  myImage = love.graphics.newImage("assets/images/sheep.png")
+
+  love.graphics.setBackgroundColor(love.math.colorFromBytes(181, 121, 121))
+
   Rectangles:load()
 
   --The first argument is a function
@@ -17,14 +22,17 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.setColor(love.math.colorFromBytes(255, 200, 40, 127))
+  love.graphics.draw(myImage, 100, 100, 45, -1, -1, 39, 50)
+
+  love.graphics.setColor(love.math.colorFromBytes(255, 255, 255))
+  love.graphics.print("Hello World", 400, 300, 25, 3, 3)
 
   Rectangles:drawAll()
 
   --if drawRectangle is true then draw a rectangle
   if drawRectangle then
-    love.graphics.setColor(255, 255, 255)
+    love.graphics.setColor(love.math.colorFromBytes(255, 255, 255))
     love.graphics.rectangle("fill", 100, 100, 300, 200)
   end
 end
--- lib/rectangles.lua
+++ lib/rectangles.lua
@@ -37,7 +37,7 @@ function Rectangles:updateAll(dt)
 end
 
 function Rectangle:draw(rect)
-  love.graphics.setColor(0, 0.4, 0.4)
+  love.graphics.setColor(love.math.colorFromBytes(0, 102, 102))
   love.graphics.rectangle(
     "fill",
     rect.x,
