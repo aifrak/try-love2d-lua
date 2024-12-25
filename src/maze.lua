4be4486ad3f7bbade71c53c10d5544c1fef6cfe9
aifrak <aifrak@users.noreply.github.com>
2024-12-25 18:36:20 +0100

    Example of tiles (with image)

--- src/maze.lua
+++ src/maze.lua
@@ -1,24 +1,57 @@
+local Assets = require "assets"
+
 local Maze = {}
 
+-- tables
+local quads = {}
+
 local tilemap
-local colors
+local image
+local width
+local height
 
 
 function Maze:load()
-  tilemap = {
-    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-    { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
-    { 1, 2, 3, 4, 5, 5, 4, 3, 2, 1 },
-    { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
-    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
-  }
+  image = love.graphics.newImage(Assets.image_path("tileset.png"))
 
-  colors = {
-    { 1, 1, 1 },
-    { 1, 0, 0 },
-    { 1, 0, 1 },
-    { 0, 0, 1 },
-    { 0, 1, 1 }
+  --We need the full image width and height for creating the quads
+  local image_width = image:getWidth()
+  local image_height = image:getHeight()
+
+  --The width and height of each tile is 32, 32
+  --So we could do:
+  width = 32
+  height = 32
+
+  --But let's say we don't know the width and height of a tile
+  --We can also use the number of rows and columns in the tileset
+  --Our tileset has 2 rows and 3 columns
+  --But we need to subtract 2 to make up for the empty pixels we included to prevent bleeding
+  width = (image_width / 3) - 2
+  height = (image_height / 2) - 2
+
+  for i = 0, 1 do
+    for j = 0, 2 do
+      --The only reason this code is split up in multiple lines
+      --is so that it fits the page
+      table.insert(quads,
+        love.graphics.newQuad(
+          1 + j * (width + 2),
+          1 + i * (height + 2),
+          width, height,
+          image_width, image_height))
+    end
+  end
+
+  tilemap = {
+    { 1, 6, 6, 2, 1, 6, 6, 2 },
+    { 3, 0, 0, 4, 5, 0, 0, 3 },
+    { 3, 0, 0, 0, 0, 0, 0, 3 },
+    { 4, 2, 0, 0, 0, 0, 1, 5 },
+    { 1, 5, 0, 0, 0, 0, 4, 2 },
+    { 3, 0, 0, 0, 0, 0, 0, 3 },
+    { 3, 0, 0, 1, 2, 0, 0, 3 },
+    { 4, 6, 6, 5, 4, 6, 6, 5 }
   }
 
   return self
@@ -28,8 +61,7 @@ function Maze:draw()
   for i, row in ipairs(tilemap) do
     for j, tile in ipairs(row) do
       if tile ~= 0 then
-        love.graphics.setColor(colors[tile])
-        love.graphics.rectangle("fill", j * 25, i * 25, 25, 25)
+        love.graphics.draw(image, quads[tile], j * width, i * height)
       end
     end
   end
